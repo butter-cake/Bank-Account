2f9e41e5c49505f6f3a6021d66c26adfcfe4a1d5
butter-cake <ernchae@gmail.com>
2023-06-11 00:29:54 -0700

    first commit

---
+++ bankAccount.py
@@ -0,0 +1,32 @@
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate, balance): 
+        self.int_rate = int_rate
+        self.balance = balance
+        # your code here! (remember, instance attributes go here)
+        # don't worry about user info here; we'll involve the User class soon
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance - amount < 0:
+            print("insufficient funds: Charging a $5 fee")
+            self.balance = self.balance  - 5
+            return self
+        self.balance = self.balance  - amount
+        return self
+
+    def display_account_info(self):
+        print("Balance: ", self.balance)
+
+    def yield_interest(self):
+        if(self.balance > 0):
+            self.balance = self.balance*self.int_rate
+            return self
+        return self
+b1 = BankAccount(1.05, 5000).deposit(1500).deposit(250).deposit(3000).withdraw(2500).yield_interest().display_account_info()
+
+b2 = BankAccount(1.08, 2500).deposit(2000).deposit(1000).withdraw(1000).withdraw(500).withdraw(1250).withdraw(100).yield_interest().display_account_info()
+
+
